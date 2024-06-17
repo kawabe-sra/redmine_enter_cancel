8151c370ec149bbc87213d37f3ef4e90cb37a59f
Y. Kawabe <kawabe@sra.co.jp>
2024-06-17 10:44:28 +0900

    fix for Redmine 5.x.

--- init.rb
+++ init.rb
@@ -1,10 +1,10 @@
-require 'enter_cancel_listener'
+require File.expand_path('../lib/enter_cancel_listener', __FILE__)
 
 Redmine::Plugin.register :redmine_enter_cancel do
   name 'Redmine Enter Cancel plugin'
   author 'suer'
   description 'prevent unintended ticket creation by enter key'
-  version '0.0.2'
+  version '0.0.2a'
   url 'https://github.com/suer/redmine_enter_cancel'
   author_url 'http://d.hatena.ne.jp/suer'
 end
